45cc40608c966e5c4262aa2add62e255598954ae
Mike Heavers <mheavers@gmail.com>
2020-03-25 14:38:13 -0700

    not sure...

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,30 @@
+let client;
+let page;
+
 const mainModule = (function(document) {
-  this.page = document.querySelector('body').id;
 
   init = function() {
+    console.log(contentful);
+    configureContentful();
+    getData();
     addSiteListeners();
+  }
+
+  configureContentful = function(){
+    client = contentful.createClient({
+      space: '1lkiepv11wqc',
+      accessToken: 'RwxLp1wT6n025alHYLR6Gtj1aDjyVzfHDw4tqbDHiXs'
+    });
+    page = document.querySelector('body').id;
+  }
+
+  getData = function(){
+    client.getEntry('2EvEQlSrxwzhucJ3Jcn2eh')
+    .then(function (entry) {
+      console.log(entry.fields);
+      document.querySelector('.page-intro-line-one').innerHTML = entry.fields.introTitleLineOne;
+      document.querySelector('.page-intro-line-two').innerHTML = entry.fields.introTitleLineTwo;
+    });
   }
 
   onSiteNavToggleClick = (e)=>{
